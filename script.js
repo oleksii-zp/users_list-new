b286a7bc00202a59347c55cd6fea414de934add1
oleksii-zp <oleksii.podkolzin@gmail.com>
2017-04-23 14:45:51 +0300

    solved some issues of editform opening, date, events

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
+$(document).ready(function(){
+  $('#button-add-users').on('click', addUsers);
+  $('#button-save').on('click', save);
+  $('.search').on('keyup', searchBy);
+  $('.sortable').on('click', sortTable);
+  $('#button-remove-user').on('click', removeUser);
+  $('#userForm').on('submit', editUser);
+
+
+
 var arr;
 // console.log(arr);
 
-$(document).ready(function() {
+
     arr = getFromLocStor();
     console.log(arr);
-      arr.forEach(function(item, i) {
-        // console.log(arr[i]);
+    arr.forEach(function(item, i) {
+        console.log(arr[i]);
         insertTemplate(arr[i]);
-      })
+    });
 
-});
 
 function getFromLocStor() {
     var returnObj = JSON.parse(localStorage.getItem("allUsers"));
 
     if (returnObj == null || returnObj.length === 0) {
-        var divAlert = document.createElement('div');
-        divAlert.className = 'alert alert-info text-center';
-        divAlert.innerHTML = '<strong>Sorry!</strong> No users found.';
-        $('#tableContainer').append(divAlert);
-        return returnObj = [];
+        // var divAlert = document.createElement('div');
+        // divAlert.className = 'alert alert-info text-center';
+        // divAlert.innerHTML = '<strong>Sorry!</strong> No users found.';
+        $("<div class = 'alert alert-info text-center'>Sorry! No users found.</div>").appendTo('#tableContainer');
+        console.log();
+        return [];
     } else {
-        return returnObj
-    }
+        return returnObj;
+    };
 }
+
+function insertTemplate(user, editTr, tr) {
+    var compiled = _.template($("#rows-template").html())(user);
+    var firstrow = $($(compiled)[0]).on('click', hideShow);
+    var secondrow = $(compiled)[2];
+    $(secondrow).find('.btn:first').on('click', openEditForm);
+    $(secondrow).find('.btn:last').on('click', openConfirm);
+    if (editTr) {$(tr).before(firstrow, secondrow);}
+    else {$("#myTbody").append(firstrow).append(secondrow);}
+
+}
+
 
 function addUsers() {
     $.ajax({
@@ -33,36 +55,38 @@ function addUsers() {
             // console.log(data);
             $('.alert-info').remove();
             data.results.forEach(function(item, i) {
-              var randomUser = data.results[i];
-              var newUser = {
-                userId: randomUser.id.value,
-                first: _.startCase(randomUser.name.first),
-                last: _.startCase(randomUser.name.last),
-                gender: randomUser.gender,
-                get genderImg() {
-                  if (this.gender === 'female') {
-                    return './img/woman.png';
-                  }
-                  if (this.gender === 'male') {
-                    return './img/man.png';
-                  }
-                },
-                birthday: changeDate(randomUser.dob),
-                username: randomUser.login.username,
-                email: randomUser.email,
-                location: _.startCase(randomUser.location.state),
-                zipcode: randomUser.location.postcode,
-                city: _.startCase(randomUser.location.city),
-                address: _.startCase(randomUser.location.street),
-                phone: randomUser.phone,
-                cell: randomUser.cell,
-                registered: changeDate(randomUser.registered),
-                lrgThumbnail: randomUser.picture.large
-              }
-              // console.log(newUser);
-              arr.push(newUser);
-              // console.log(arr);
-              insertTemplate(newUser);
+                var randomUser = data.results[i];
+                var newUser = {
+                    userId: randomUser.id.value,
+                    first: _.startCase(randomUser.name.first),
+                    last: _.startCase(randomUser.name.last),
+                    gender: randomUser.gender,
+                    get genderImg() {
+                        if (this.gender === 'female') {
+                            return './img/woman.png';
+                        }
+                        if (this.gender === 'male') {
+                            return './img/man.png';
+                        }
+                    },
+                    birthday: changeDate(randomUser.dob),
+                    birthdayOrig: new Date(randomUser.dob),
+                    username: randomUser.login.username,
+                    email: randomUser.email,
+                    location: _.startCase(randomUser.location.state),
+                    zipcode: randomUser.location.postcode,
+                    city: _.startCase(randomUser.location.city),
+                    address: _.startCase(randomUser.location.street),
+                    phone: randomUser.phone,
+                    cell: randomUser.cell,
+                    registered: changeDate(randomUser.registered),
+                    registeredOrig: new Date(randomUser.registered),
+                    lrgThumbnail: randomUser.picture.large
+                }
+                // console.log(newUser);
+                arr.push(newUser);
+                console.log(arr);
+                insertTemplate(newUser);
             });
         }
     });
@@ -73,25 +97,17 @@ function save() {
     localStorage.setItem("allUsers", userObj);
 }
 
-function insertTemplate(user, editTr, tr) {
-  var rowsTemplate = $("#rows-template").html();
-  // console.log(rowsTemplate);
-  // console.log(user);
-  if (editTr) {
 
-    return $(_.template(rowsTemplate)(user)).insertBefore(tr);}
-  else {return $("#myTbody").append(_.template(rowsTemplate)(user));}
-}
 
 var userIdToDel;
 function openConfirm() {
-  var button = event.target;
-  userIdToDel = $(button).data('userId');
-  arr.forEach(function(item, i) {
-      if (arr[i].userId === userIdToDel) {
-          $('#confirmMessage').text(arr[i].first + " " + arr[i].last).css('color', '#337ab7');
-      }
-  })
+    var button = event.target;
+    userIdToDel = $(button).data('userId');
+    arr.forEach(function(item, i) {
+        if (arr[i].userId === userIdToDel) {
+            $('#confirmMessage').text(arr[i].first + " " + arr[i].last).css('color', '#337ab7');
+        }
+    })
 
 }
 
@@ -111,13 +127,14 @@ function removeUser() {
 
 var currentUserId;
 function openEditForm() {
+  var button = event.currentTarget;
+  // console.log(button);
+  currentUserId = $(button).data('userId');
+  // console.log(currentUserId);
     var form = document.getElementById('userForm');
     form.reset();
 
-    var button = event.target;
-    // console.log(button);
-    currentUserId = $(button).data('userId');
-    // console.log(currentUserId);
+
     arr.forEach(function(item, i) {
         if (arr[i].userId === currentUserId) {
             $('#first').val(arr[i].first);
@@ -127,7 +144,7 @@ function openEditForm() {
             } else {
                 $(':radio[value=male]').prop('checked', true);
             }
-            $('#birthday').prop('valueAsDate', new Date(arr[i].birthday));
+            $('#birthday').prop('valueAsDate', new Date(arr[i].birthdayOrig));
             $('#username').val(arr[i].username);
             $('#email').val(arr[i].email);
             $('#location').val(arr[i].location);
@@ -136,62 +153,63 @@ function openEditForm() {
             $('#address').val(arr[i].address);
             $('#phone').val(arr[i].phone);
             $('#cell').val(arr[i].cell);
-            $('#registered').prop('valueAsDate', new Date(arr[i].registered));
+            $('#registered').prop('valueAsDate', new Date(arr[i].registeredOrig));
             $('#thumbnailInForm').attr('src', arr[i].lrgThumbnail);
             $('#thumbnailInForm').off('click');
             $('#thumbnailInForm').click(function() {
-              getRandomThumbnail($('input[name=gender]:checked').val());
+                getRandomThumbnail($('input[name=gender]:checked').val());
             })
         }
 
     });
-  }
+}
 
-  function editUser() {
-    // event.preventDefault();
+function editUser() {
+    event.preventDefault();
     console.log(currentUserId);
     var button = $("div").find("[data-user-id='" + currentUserId + "']");
     var trToEdit = $(button).closest('.hiddenrow');
     var prevTrToEdit = $(trToEdit).prev('.firstrow');
-  console.log(trToEdit);
-  console.log(prevTrToEdit);
+    console.log(trToEdit);
+    console.log(prevTrToEdit);
     arr.forEach(function(item, i) {
         if (arr[i].userId === currentUserId) {
-          arr[i] = {
-          userId: currentUserId,
-          first: _.startCase($('#first').val()),
-          last: _.startCase($('#last').val()),
-          gender: $('input[name=gender]:checked').val(),
-          get genderImg() {
-            if (this.gender === 'female') {
-              return './img/woman.png';
-            }
-            if (this.gender === 'male') {
-              return './img/man.png';
-            }
-          },
-          birthday: changeDate($('#birthday').val()),
-          username: $('#username').val(),
-
-          email: $('#email').val(),
-          location: _.startCase($('#location').val()),
-          zipcode: $('#zipcode').val(),
-          city: _.startCase($('#city').val()),
-          address: _.startCase($('#address').val()),
-          phone: $('#phone').val(),
-          cell: $('#cell').val(),
-          registered: changeDate($('#registered').val()),
-          lrgThumbnail: $('#thumbnailInForm').attr('src')
-        };
-        insertTemplate(arr[i], true, prevTrToEdit);
-        $(trToEdit[0]).remove();
-        $(prevTrToEdit[0]).remove();
-        $('#myModal').modal('hide');
-                }
+            arr[i] = {
+                userId: currentUserId,
+                first: _.startCase($('#first').val()),
+                last: _.startCase($('#last').val()),
+                gender: $('input[name=gender]:checked').val(),
+                get genderImg() {
+                    if (this.gender === 'female') {
+                        return './img/woman.png';
+                    }
+                    if (this.gender === 'male') {
+                        return './img/man.png';
+                    }
+                },
+                birthday: changeDate($('#birthday').val()),
+                birthdayOrig: $('#birthday').prop('valueAsDate'),
+                username: $('#username').val(),
+                email: $('#email').val(),
+                location: _.startCase($('#location').val()),
+                zipcode: $('#zipcode').val(),
+                city: _.startCase($('#city').val()),
+                address: _.startCase($('#address').val()),
+                phone: $('#phone').val(),
+                cell: $('#cell').val(),
+                registered: changeDate($('#registered').val()),
+                registeredOrig: $('#registered').prop('valueAsDate'),
+                lrgThumbnail: $('#thumbnailInForm').attr('src')
+            };
+            insertTemplate(arr[i], true, prevTrToEdit);
+            $(trToEdit[0]).remove();
+            $(prevTrToEdit[0]).remove();
+            $('#myModal').modal('hide');
+        }
 
     })
     console.log(arr);
-  }
+}
 
 // function countFemale() {
 //     var numFemale = 0;
@@ -213,17 +231,15 @@ function openEditForm() {
 //     return numMale;
 // };
 
-
 function changeDate(myDate) {
     var someDate = new Date(myDate);
-    var options = {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit'
-    };
-    return someDate.toLocaleDateString('en-US', options);
+    // var options = {
+    //     year: 'numeric',
+    //     month:'2-digit',
+    //     day: '2-digit'
+    // };
+    return someDate.toLocaleDateString();
 };
-
 
 function searchBy() {
     $('.divInfo').css('display', 'none');
@@ -265,7 +281,7 @@ function hideShow() {
     var tr = event.currentTarget;
     var plMin = $(tr).find('.plMin');
     var divToHide = $(tr).next().find('.divInfo');
-      if (divToHide[0].style.display == 'none') {
+    if (divToHide[0].style.display == 'none') {
         $('.divInfo').slideUp();
         $('.plMin').css('backgroundImage', 'url(./img/plus.png)');
         $(divToHide[0]).slideDown();
@@ -276,7 +292,7 @@ function hideShow() {
     };
 }
 
-function sortTable(n) {
+function sortTable() {
     var table,
         rows,
         switching,
@@ -286,6 +302,9 @@ function sortTable(n) {
         shouldSwitch,
         dir,
         switchcount = 0;
+        var thclick = event.currentTarget;
+        console.log(thclick);
+        var n = thclick.cellIndex;
     table = document.getElementById("myTable");
     switching = true;
     dir = "asc";
@@ -321,26 +340,27 @@ function sortTable(n) {
             }
         }
     }
-    var thsort = $('.sortable');
-    $(thsort).css('backgroundImage', 'url(./img/sort.png)')
+    // var thsort = $('.sortable');
+    $('.sortable').css('backgroundImage', 'url(./img/sort.png)')
     if (dir == 'asc') {
-        $(thsort[n - 1]).css('backgroundImage', 'url(./img/sortasc.png)')
+        $(thclick).css('backgroundImage', 'url(./img/sortasc.png)')
     }
     if (dir == 'desc') {
-        $(thsort[n - 1]).css('backgroundImage', 'url(./img/sortdesc.png)')
+        $(thclick).css('backgroundImage', 'url(./img/sortdesc.png)')
     }
 }
 
-
 function getRandomThumbnail(gender) {
-  console.log('a');
+    console.log('a');
     $.ajax({
-        url: 'https://randomuser.me/api/?nat=us&results=1&gender='+gender,
+        url: 'https://randomuser.me/api/?nat=us&results=1&gender=' + gender,
         dataType: 'json',
         success: function(data) {
-          console.log(data);
-              $('#thumbnailInForm').attr('src', data.results[0].picture.large);
-              // return data.results[0].picture.large
-              }
-            });
+            console.log(data);
+            $('#thumbnailInForm').attr('src', data.results[0].picture.large);
+            // return data.results[0].picture.large
         }
+    });
+}
+
+})
